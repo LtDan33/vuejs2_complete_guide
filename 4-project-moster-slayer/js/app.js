8422d172f41d2378ebc3a4adf77ab28d82b38fda
Dan <wslinebacker@gmail.com>
2019-03-31 21:38:13 -0500

    Added styling for monster and player. Correcte endgame bug

--- 4-project-moster-slayer/js/app.js
+++ 4-project-moster-slayer/js/app.js
@@ -19,17 +19,19 @@ new Vue({
         this.gameIsRunning = true;
         this.playerHealth = 100;
         this.monsterHealth = 100;
+        this.turns = [];
     },
     attack: function(){
         damage = this.calcDamage(3,10);
         this.monsterHealth -= damage;
         this.turns.unshift({
-            // isPlayer = true,
+            isPlayer : true,
             text: 'Player hits monster for ' + damage
         });
 
-
-        this.checkIfWon();
+        if(this.checkIfGameOver()){
+            return;
+        }
         this.monsterAttack();
         
     },
@@ -38,42 +40,47 @@ new Vue({
         this.playerHealth -= damage;
 
         this.turns.unshift({
-            // isPlayer = false,
+            isPlayer : false,
             text: 'Monster hits player for ' + damage
         });
 
-        this.checkIfWon();
+        this.checkIfGameOver();
     },
 
     calcDamage : function(min, max){
        return Math.max(Math.floor(Math.random() * max) + 1 , min); 
     },
 
-    checkIfWon : function(){
+    checkIfGameOver : function(){
         if (this.monsterHealth <= 0){
             if(confirm('You won. New Game?')){
                 this.startGame();
             } else {
                 this.gameIsRunning = false;
             }
+            return true;
         } else if (this.playerHealth <= 0){
             if(confirm('You lost. New Game?')){
                 this.startGame();
             } else {
                 this.gameIsRunning = false;
             }
+            return true;
         }
+        return false;
     },
 
     specialAttack: function(){
         damage = this.calcDamage(10,20);
         this.monsterHealth -= damage;
         this.turns.unshift({
-            // isPlayer = true,
-            text: 'Player hits monster for ' + damage
+            isPlayer : true,
+            text: 'Player hits monster hard for ' + damage
         });
 
-        this.checkIfWon();
+        if(this.checkIfGameOver()){
+            return;
+        }
         this.monsterAttack();
         
     },
@@ -89,16 +96,18 @@ new Vue({
             this.playerHealth += heal;
         }
         else{
-            heal = "full health" 
+            heal = "full health";
             this.playerHealth = 100;
         }
 
         this.turns.unshift({
-            // isPlayer = true,
+            isPlayer : true,
             text: 'Player heals for ' + heal
         });
 
-        this.checkIfWon();
+        if(this.checkIfGameOver()){
+            return;
+        }
         this.monsterAttack();
     },
     giveUp: function(){
